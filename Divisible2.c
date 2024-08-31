5bc56815c65658d9efa8c39f6a757c2e343ccd67
uzzalCse19 <uzzalhasan7878@gmail.com>
2024-09-01 00:40:27 +0600

    Introduction_C

---
+++ Divisible2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=21; i<=n; i++)
+    {
+        if(i%3==0 && i%7==0) printf("%d\n",i);
+    }
+    return 0;
+}
+
---
+++ Multiply.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+   long long int a,b;
+    scanf("%lld %lld",&a,&b);
+    printf("%lld\n",a*b);
+    return 0;
+}
